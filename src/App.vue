a3692e73fd058a690b52555939714d68b7cdab0a
Pablo Quipildor <pabloquipildor98@gmail.com>
2023-11-20 05:47:58 -0300

    primer subida

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <nav>
+
+      <router-link to="/about">Inicio</router-link>|
+      <router-link to="/">Favorito</router-link> 
+      
+    </nav>
+    <router-view/>
+
+    <header>
+        <form id="form">
+            <input type="text" placeholder="Search" id="search" class="search">
+        </form>
+    </header>
+
+    <main id="main"> </main>
+
+    <footer class="container">
+
+<div class="footer-credits">
+    <p class="paragraph text-title">© 2023 Emae.</p>
+    <p class="paragraph text-title">Roque Mamani | Pablo Quipildor.</p>
+</div>
+
+
+
+</footer>
+
+    
+  </div>
+
+
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1 class="titulo-tmdb"> Populares</h1>
+<mostrar-peli>
+    
+</mostrar-peli>
+    
+  </div>
+
+  
+
+</template>
+
+<script>
+import MostrarPeli from '../components/MostarPeli.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    MostrarPeli
+  }
+}
+</script>
